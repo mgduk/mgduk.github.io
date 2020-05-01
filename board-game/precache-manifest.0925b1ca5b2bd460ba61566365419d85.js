89d963bda4e634ff745dec2622caaa5de9ad1859
Matt Dolan <mdolan@trello.com>
2020-05-01 11:52:09 +0100

    Fix board game root

---
+++ board-game/precache-manifest.0925b1ca5b2bd460ba61566365419d85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ba3f37c9e75f075111269a34b43ef31",
+    "url": "/board-game/index.html"
+  },
+  {
+    "revision": "714088c913072c396618",
+    "url": "/board-game/static/css/main.53f727c3.chunk.css"
+  },
+  {
+    "revision": "5fb140d2618ac7b7af5e",
+    "url": "/board-game/static/js/2.8c4fe231.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/board-game/static/js/2.8c4fe231.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "714088c913072c396618",
+    "url": "/board-game/static/js/main.d53a0f85.chunk.js"
+  },
+  {
+    "revision": "5aa47dbf2e10ab198074",
+    "url": "/board-game/static/js/runtime-main.58bda06c.js"
+  }
+]);
--- board-game/service-worker.js
+++ board-game/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7a523499c039944a416ba23a0091710c.js"
+  "/board-game/precache-manifest.0925b1ca5b2bd460ba61566365419d85.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/board-game/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
