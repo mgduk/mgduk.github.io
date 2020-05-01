a32331cb88150a0a13c92be75e33f29439f87545
Matt Dolan <mdolan@trello.com>
2020-05-01 11:48:38 +0100

    Add board game properly

---
+++ board-game/precache-manifest.7a523499c039944a416ba23a0091710c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f818401a14cafb4dbd08b087526faa9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83407cf67d7dd855b940",
+    "url": "/static/css/main.53f727c3.chunk.css"
+  },
+  {
+    "revision": "5fb140d2618ac7b7af5e",
+    "url": "/static/js/2.8c4fe231.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.8c4fe231.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83407cf67d7dd855b940",
+    "url": "/static/js/main.def8acc9.chunk.js"
+  },
+  {
+    "revision": "8b49166c18ded66e08d3",
+    "url": "/static/js/runtime-main.44a7ec63.js"
+  }
+]);
